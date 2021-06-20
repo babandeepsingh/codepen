6c92f9ef7fd666ef5d61f8819a65e677eaed3e6f
Babandeep Singh <er.babandeep@gmail.com>
2021-06-21 04:45:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
-function showPreview(){
-  var htmlCode = document.getElementById("htmlCode").value;
-  var cssCode = "style"+document.getElementById("htmlCode").value+"</style>;
-  var jsCode = ""+document.getElementById("jsCode").value+"";
-  var frame = document.getElementById("preview-window").contentWindow.document;
-  frame.open();
-  frame.write(htmlCode+cssCode+jsCode);
-  frame.close();
+window.onload = function () {
+  var d = document;
+  var q = "querySelector";
+  var iframe = d[q]("iframe");
+  // We target all textarea
+  var area = document.querySelectorAll('textarea');
+
+  // We bind on each textarea change event
+  for (var i = area.length - 1; i >= 0; i--) {
+    if (area[i].addEventListener) {
+      area[i].addEventListener('input', function () {
+        // event handling code for sane browsers
+        iframe.srcdoc = area[0].value + '<style>' +
+          area[1].value + '</style>' + '<script>' +
+          area[2].value + '<\/script>';
+      }, false);
+    } else if (area[i].attachEvent) {
+      area[i].attachEvent('onpropertychange', function () {
+        // IE-specific event handling code
+        iframe.srcdoc = area[0].value + '<style>' +
+          area[1].value + '</style>' + '<script>' +
+          area[2].value + '<\/script>';
+      });
+    }
+  }
 }
-document.getElementById("htmlCode").addEventListener("keyup", showPreview)
-document.getElementById("jsCode").addEventListener("keyup", showPreview)
-document.getElementById("cssCode").addEventListener("keyup", showPreview)
